3b2d890d432017adbb70231585d9e4a34f6bb3f2
Sathvik <99279970+rssnama@users.noreply.github.com>
2023-07-12 12:56:42 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,71 +3,34 @@ from matplotlib import pyplot as plt
 import pytesseract
 from PIL import Image
 from flask import Flask, request, render_template, redirect,jsonify
-import numpy as np
 import urllib.request
-#from firebase_admin import credentials, firestore, initialize_app
 
 app = Flask(__name__)
-#cred = credentials.Certificate('key.json')
-#default_app = initialize_app(cred)
-#db = firestore.client()
-#todo_ref = db.collection('todos')
-
-#app.config["IMAGE_UPLOADS"] = "C:/Users/Sathvik/Downloads/OCR/images"
-# app.config["ALLOWED_IMAGE_EXTENSIONS"] = ["PNG","JPG","JPEG"]
-
 from werkzeug.utils import secure_filename
 
-
-#image_file = "images/vc2.png"
-#img = cv2.imread(image_file)
 @app.route('/', methods=["GET", "POST"])
-
 
 def upload_image():
 
     global img
-    reqRef = request.get_json(force=True)
-    img = reqRef["imgUrl"]
-    urllib.request.urlretrieve(img,"gfg.png")
-    # img = Image.open("gfg.png")
+    try:
+        reqRef = request.get_json(force=True)
+        img = reqRef["imgUrl"]
+        urllib.request.urlretrieve(img,"gfg.jpeg")
+    # img = Image.open("gfg.jpeg")
     # img.show()
 
     # print("abc"+ img)
-    try:
+    
         
         
-        inverted = 0
-        img = cv2.imread("gfg.png")
-
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("gray.jpg", gray)
-
-        thresh, im_bw = cv2.threshold(img, 210, 230, cv2.THRESH_BINARY)
-        cv2.imwrite("bw_image.jpg", im_bw)
-
-        # def noise_removal(image):
-        kernel = np.ones((1,1), np.uint8)
-        im_bw = cv2.dilate(im_bw, kernel, iterations=1)
-        kernel = np.ones((1, 1), np.uint8)
-        im_bw = cv2.erode(im_bw, kernel, iterations=1)
-        im_bw = cv2.morphologyEx(im_bw, cv2.MORPH_CLOSE, kernel)
-        im_bw = cv2.medianBlur(im_bw, 3)
-            # return (image)
-        
-        no_noise = im_bw
-        cv2.imwrite("no_noise.jpg", no_noise)
-
-        img_file = "vc2.png"
-        no_noise = "no_noise.jpg"
-
-        img = Image.open(no_noise)
-        ocr_result = pytesseract.image_to_string(img)
-
-        #print(ocr_result)
+        path_to_tesseract=r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
+        #Imagepath='gfg.jpeg'
+        pytesseract.tesseract_cmd=path_to_tesseract
+        text=pytesseract.image_to_string(Image.open("gfg.jpeg"))
 
         return jsonify({
-            "ocr_result": ocr_result,
+            "ocr_result": text,
 
         })
 
@@ -84,4 +47,4 @@ def upload_image():
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0')
+    app.run(debug=False, port='0.0.0.0')
